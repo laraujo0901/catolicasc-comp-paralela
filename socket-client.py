16eaf537a8d968fcad9111613a5315c9e3cc244a
Luciano de Araujo <l.araujo0901@gmail.com>
2019-06-15 11:18:46 -0300

    Adicionando multithread nos exemplos de socket

--- socket-client.py
+++ socket-client.py
@@ -1,18 +1,25 @@
-import socket
+import socket, time, sys
 
 HOST = '127.0.0.1'
 PORT = 65431
 
 print("Iniciando cliente...")
 
+if (len(sys.argv) == 4):
+    ID = sys.argv[1]
+    HOST = sys.argv[2]
+    PORT = int(sys.argv[3])
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Conectando ao servidor {}, port {}".format(HOST,PORT))
     s.connect((HOST, PORT))
     
-    print("Enviando dados para o servidor...")
-    s.sendall(b'Hello, world')
+    print("Iniciando ping-ping com o servidor...")
 
-    print("Recebendo dados do servidor...")
-    data = s.recv(1024)
-
-print('Recebido: ', repr(data))
+    while True:
+        print("ping...")
+        msg = f'ping {ID}'
+        s.sendall(str.encode(msg))
+        data = s.recv(1024)
+        print(repr(data))
+        time.sleep(1)   
--- socket-server.py
+++ socket-server.py
@@ -1,22 +1,40 @@
-import socket
+import socket, sys
+from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 65431
+
+if len(sys.argv) > 3:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+def manage_conn(conn, addr):
+    with conn:
+        print("Conexão recebida de {}. Vamos conversar...".format(addr))
+        while True:
+            data = conn.recv(1024)
+            print("Recebido de {} ".format(addr), repr(data))
+            if not data:
+                print("Sem mais dados. Encerrando conexão!")
+                break
+            print("Enviando pong!")
+            conn.sendall(b'pong!')
+    
 
 print("Iniciando servidor socket...")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Realizando binding com servidor {}, porta {}...".format(HOST, PORT))
     s.bind((HOST, PORT))
+
     print("Escutando porta...")
     s.listen()
-    print("Aguardando conexões...")
-    conn, addr = s.accept()
-    with conn:
-        print("Conexão recebida de {}. Vamos conversar...".format(addr))
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("Sem mais dados. Encerrando conexão!")
-                break
-            conn.sendall(data)
+
+    while True:
+        print("Aguardando conexões...")
+        conn, addr = s.accept()
+        
+        # criar nova thread
+        thread = Thread(target=manage_conn, args=(conn,addr))
+        thread.start()
+        
